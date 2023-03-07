26ede41f032a02cefbc556f013a68c4e34619138
hndrbrm <hndrbrm@gmail.com>
2023-03-07 09:17:03 +0700

    refactor location model into smaller part using mixin.

--- lib/quran/location.dart
+++ lib/quran/location.dart
@@ -2,29 +2,40 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-class Location {
-  const Location(this.surah, this.ayah)
+abstract class _Location {
+  const _Location(this.surah, this.ayah)
     : assert(1 <= surah && surah <= 114);
-
-  Location.fromJson(Map<String, dynamic> json)
-    : surah = json['surah'],
-      ayah = json['ayah'];
 
   final int surah;
   final int ayah;
+}
 
-  Map<String, dynamic> toJson() => {
-    'surah': surah,
-    'ayah': ayah,
-  };
+class Location extends _Location with _ToJson, _ToString, _Equality {
+  const Location(super.surah, super.ayah);
 
+  Location.fromJson(Map<String, dynamic> json)
+    : super(json['surah'], json['ayah']);
+}
+
+mixin _ToJson on _Location {
+  Map<String, dynamic> toJson() =>
+    {
+      'surah': surah,
+      'ayah': ayah,
+    };
+}
+
+mixin _ToString on _ToJson {
   @override
-  bool operator ==(Object other) {
-    return
-      other is Location &&
-      other.surah == surah &&
-      other.ayah == ayah;
-  }
+  String toString() => toJson().toString();
+}
+
+mixin _Equality on _Location {
+  @override
+  bool operator ==(Object other) =>
+    other is Location &&
+    other.surah == surah &&
+    other.ayah == ayah;
 
   @override
   int get hashCode => Object.hash(surah, ayah);
